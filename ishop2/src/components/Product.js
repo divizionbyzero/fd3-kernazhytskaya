65f6eaf244f5063ae27f874a1aa7d515d58a7247
Alena Kernazhytskaya <akernazhytskaya@1stquad.com>
2019-05-29 14:59:29 +0300

    second homework ishop2

--- ishop2/src/components/Product.js
+++ ishop2/src/components/Product.js
@@ -2,15 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class Product extends React.Component {
+
   render() {
-    const { name, price, count, src } = this.props.product;
+    const { name, price, count, src, id } = this.props.product;
+    const { clickHandler, delHandler, selectedId } = this.props;
 
     return (
-      <div className="product">
+      <div onClick={() => clickHandler(id)} className={selectedId === id ? "product product-selected" : "product"}>
         <div className="product-photo-wrap">
           <img className="product-photo" src={src} alt={name} />
         </div>
-        <h3 className="product-name">{name}</h3>
+        <div className="product-top">
+          <h3 className="product-name">{name}</h3>
+          <button onClick={() => delHandler(id)}>Del</button>
+        </div>
         <div className="product-info">
           <span className="product-price">{price}</span>
           <span className="product-count">count: {count}</span>
@@ -22,12 +27,15 @@ class Product extends React.Component {
 
 Product.propTypes = {
   product: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
     src: PropTypes.string.isRequired,
-    count: PropTypes.number.isRequired
-  })
-};
+    count: PropTypes.string.isRequired
+  }),
+  selectedId: PropTypes.string,
+  clickHandler: PropTypes.func,
+  delHandler: PropTypes.func
+}
 
 export default Product;
--- ishop2/src/components/ProductsList.js
+++ ishop2/src/components/ProductsList.js
@@ -4,14 +4,38 @@ import PropTypes from "prop-types";
 import Product from "./Product";
 
 class ProductsList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        selectedId: '',
+        products: this.props.products
+    };
+  }
+
+  toggleSelected = (id) => {
+    this.setState({selectedId: id});
+  }
+
+  delHandler = (id) => {
+    if (window.confirm('Are you sure you wish to delete this item?'))
+    this.setState({products: this.state.products.filter(product => product.id !== id)});
+  }
+
+
   render() {
-    const { products, shopName } = this.props;
+    const { shopName } = this.props;
+    const { products } = this.state;
     return (
       <div>
         <h2>{shopName}</h2>
         <div className="product-list">
           {products.map(item => (
-            <Product key={item.id} product={item} />
+            <Product key={item.id}
+                      product={item}
+                      clickHandler={this.toggleSelected}
+                      delHandler={this.delHandler}
+                      selectedId={this.state.selectedId} />
           ))}
         </div>
       </div>
